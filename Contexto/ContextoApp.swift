53ced4880535fb4721553ae9fe606934cafb377e
Andreas Ink <andreasink17@gmail.com>
2022-04-25 23:55:32 -0400

    Initial Commit

---
+++ Contexto/ContextoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextoApp.swift
+//  Contexto
+//
+//  Created by Andreas Ink on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ContextoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
